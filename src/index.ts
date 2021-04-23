f7e157648b3fbae668b1fcbd1dcecc725411f4da
Kalinin Dmitry <vega-kim@mail.ru>
2021-04-23 14:09:20 +0300

    enum

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log('Hi There');
+import fs from 'fs';
+
+const matches = fs
+  .readFileSync(`${__dirname}/../football.csv`, 'utf-8')
+  .split('\n')
+  .map((row: string): string[] => {
+    return row.split(',');
+  });
+
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+}
+
+let manUnitedWins = 0;
+
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(manUnitedWins);
